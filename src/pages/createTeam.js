b2f10ed2f91a15fd16269f460f55dfb6016e9340
JuanFeBlancoT <juanfeblancot@gmail.com>
2022-05-16 18:10:35 -0500

    Inital skeleton for createTeam interface

---
+++ src/pages/createTeam.js
@@ -0,0 +1,25 @@
+export default function Component() {
+  
+    return (
+    <div>
+      <div>
+        <div>
+          <h1>Crea un nuevo equipo</h1>
+          <h3>Vive nuevas experiencias</h3>
+          <br/>
+          <hr></hr>
+          <br/>
+          <h5>¿Cómo se llaman?</h5>
+          <input type="text"></input>
+          <h5>¿Quiénes son?</h5>
+          <div>
+            
+          </div>
+        </div>
+        <div>
+
+        </div>
+      </div>
+    </div>
+    )
+}
