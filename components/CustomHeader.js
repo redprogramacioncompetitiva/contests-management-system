be58a009252b9ed3d0a18ecb2bfdea598d579d6e
Kennet Sánchez <1006229463@u.icesi.edu.co>
2022-05-16 19:58:01 -0500

    Team item component added, header customized

---
+++ components/CustomHeader.js
@@ -0,0 +1,19 @@
+import {Component} from "react"
+import Head from 'next/head'
+
+export default class CustomHeader extends Component{
+
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <Head>
+                <title>{this.props.title}</title>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
+                <link rel="stylesheet" href="../../styles/TeamsItems.module.css"/>
+            </Head>
+        )
+    }
+}
---
+++ components/users/TeamsItem.js
@@ -0,0 +1,29 @@
+import { Component } from "react"
+import style from '../../src/styles/TeamsItems.module.css'
+
+export default class TeamsItems extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state ={
+            members : [this.props.members]
+        }
+    }
+
+    render() {
+        return (
+            <section className={style.card}>
+                <section className={style.cardBody}>
+                    <h3>{this.props.teamName}</h3>
+                    <h5>{this.props.institution}</h5>
+                    <ul>
+                        {this.state.members.map((e) =>
+                            <li>{e}</li>
+                        )}
+                    </ul>
+                </section>
+            </section>
+        )
+    }
+
+}
