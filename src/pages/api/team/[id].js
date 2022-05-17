15aee5e7ee48306c9f79338f4c962ec182dd5e8e
David Montaño <cuatro04sr@gmail.com>
2022-05-16 19:29:33 -0500

    feat: add user team back and hot fix database

--- src/pages/api/team/[id].js
+++ src/pages/api/team/[id].js
@@ -2,14 +2,14 @@ import {db} from "../../../util/database";
 
 export default async function (req, res) {
     const {method, body} = req;
-    const {id_team} = req.query;
+    const {id} = req.query;
     if (method === "POST") {
         const {userName} = body;
         const query = 'INSERT INTO USERS_TEAM (USERNAME, ID_TEAM) VALUES ($1, $2) RETURNING *';
-        const values = [userName, id_team];
+        const values = [userName, id];
         try {
             const response = await db.query(query, values);
-            return res.status(200).json(response.rows[0].json);
+            return res.status(200).json(response.rows[0]);
         } catch (e) {
             return res.status(400).json({message: e.message});
         }
