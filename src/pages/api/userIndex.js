35e01c79af48b9256b7213e2646efd221f15e4ab
Mateo <mateora73@gmail.com>
2022-05-16 19:35:57 -0500

    Added competition query

--- src/pages/api/userIndex.js
+++ src/pages/api/userIndex.js
@@ -1,5 +1,5 @@
 import {db} from '../../util/database'
-import {Team} from '../../model/classes'
+import {Team,CompetitionPreview} from '../../model/classes'
 export default async function handler(req,res){
     const {method , body} = req;
     const teamsIds = await db.query('SELECT * FROM TEAMS WHERE ID_TEAM IN (SELECT ID_TEAM FROM USERS_TEAM WHERE USERNAME = $1)',[body.username])
@@ -9,7 +9,30 @@ export default async function handler(req,res){
         var team = new Team(teamsIds.rows[index].id_team,teamsIds.rows[index].team_name,members.rows)
         teams.push(team)
     }
-    
-    res.status(200).json(teams)
+
+    var competitions = []
+    const comps = await db.query('SELECT * FROM COMPETITION')
+    for (let index = 0; index < comps.rows.length; index++) {
+        var venues = await db.query('SELECT VENUE_NAME FROM VENUE WHERE ID_VENUE IN(SELECT ID_VENUE FROM VENUE_COMPETITION WHERE ID_COMPETITION = $1)',[comps.rows[index].id_competition])
+        var newComp = new CompetitionPreview(comps.rows[index].id_competition,
+            comps.rows[index].name,
+            comps.rows[index].description,
+            comps.rows[index].start_inscription,
+            comps.rows[index].end_inscription,
+            comps.rows[index].start_date,
+            comps.rows[index].end_date,
+            comps.rows[index].team_members_max,
+            comps.rows[index].team_members_min,
+            comps.rows[index].status,
+            venues
+            )
+        competitions.push(newComp)
+    }
+
+    const data = {
+        teams: teams,
+        competitions: competitions
+    }
+    res.status(200).json(data)
 }
 
