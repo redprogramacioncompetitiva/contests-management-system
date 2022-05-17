d8249da082c1a29e7591ec90f61c2839bd159225
alejandroTTT <alejandro@twototango.co>
2022-05-16 19:32:49 -0500

    :cat: Fronted have been implemented

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,17 @@
 import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react"
+import { ThemeProvider } from '@emotion/react'
+import theme from '../theme';
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ThemeProvider theme={theme}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </ThemeProvider>
   )
 }
