1e13486bbf566d8c0e946ab0804d47100a3fc0c6
David Montaño <cuatro04sr@gmail.com>
2022-05-16 19:27:07 -0500

    fix(database): database users dll

--- src/pages/api/team/[id].js
+++ src/pages/api/team/[id].js
@@ -2,11 +2,11 @@ import {db} from "../../../util/database";
 
 export default async function (req, res) {
     const {method, body} = req;
-    const {id} = req.params;
+    const {id_team} = req.query;
     if (method === "POST") {
         const {userName} = body;
         const query = 'INSERT INTO USERS_TEAM (USERNAME, ID_TEAM) VALUES ($1, $2) RETURNING *';
-        const values = [userName, id];
+        const values = [userName, id_team];
         try {
             const response = await db.query(query, values);
             return res.status(200).json(response.rows[0].json);
