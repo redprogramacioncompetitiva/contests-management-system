db9f57bb3ec1921964c10b230baeae36750f9609
Lycanfloof <lyca2206@gmail.com>
2022-05-21 12:52:31 -0500

    Implemented password validations

--- src/pages/api/user/register.js
+++ src/pages/api/user/register.js
@@ -3,16 +3,42 @@ import db from '../../util/database'
 
 export default async function register(req, res) {
     const { method, body } = req;
-    
+
     if (method === "POST") {
         body.username = body.username.trim();
         body.email = body.email.trim();
         body.name = body.name.trim();
         body.lastname = body.lastname.trim();
 
-        let check=body.username.indexOf(" ");
+        let checkSpaces = body.username.indexOf(" ");
+        let checkMiddleSpaces = body.username != "" && checkSpaces < 0 && body.email != "" && body.name != "" && body.lastname != ""
 
-        if(body.username!="" && check<0 && body.email!="" && body.name!="" && body.lastname!=""){
+        let pwIsEqual = (body.password == body.confPassword);
+        
+        let pwHasMinLength = (body.password.length >= 6);
+        let pwHasUpperCase = false;
+        let pwHasLowerCase = false;
+        let pwHasNumber = false;
+        let pwHasSpecialCharacter = false;
+
+        for (let i = 0; i < body.password.length && pwIsEqual; i++) {
+            if (body.password.charAt(i) === body.password.charAt(i).toUpperCase()) {
+                pwHasUpperCase = true;
+            }
+            if (body.password.charAt(i) === body.password.charAt(i).toLowerCase()) {
+                pwHasLowerCase = true;
+            }
+            if (!isNaN(body.password.charAt(i))) {
+                pwHasNumber = true;
+            }
+            if (body.password.charAt(i) === '.' || body.password.charAt(i) === '-' || body.password.charAt(i) === '*') {
+                pwHasSpecialCharacter = true;
+            }
+        }
+
+        let pwValidations = pwIsEqual && pwHasMinLength && pwHasUpperCase && pwHasLowerCase && pwHasNumber && pwHasSpecialCharacter
+
+        if (checkMiddleSpaces && pwValidations) {
             let response = await db.query('SELECT USERNAME FROM USERS WHERE USERNAME = $1', [req.body.username])
             console.log(response.rows)
 
@@ -30,7 +56,7 @@ export default async function register(req, res) {
                 } catch (error) {
                     console.log(error)
                 }
-             }
+            }
         }
     } else {
         res.status(404)
