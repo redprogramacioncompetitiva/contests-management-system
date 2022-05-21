1171b1d295cb071dc331f7cda29601de8dff2152
DianaOlanoU <1005967885@u.icesi.edu.co>
2022-05-21 12:37:44 -0500

    space method added

--- src/pages/api/user/register.js
+++ src/pages/api/user/register.js
@@ -2,29 +2,37 @@ import bcrypt from 'bcrypt'
 import db from '../../util/database'
 
 export default async function register(req, res) {
-    let response = await db.query('SELECT USERNAME FROM USERS WHERE USERNAME = $1', [req.body.username])
-    console.log(response.rows)
-
-    let db1 = response.rows;
     const { method, body } = req;
-
+    
     if (method === "POST") {
-        var exit = false
-        if (db1[0] === null) {
-            exit = true
-        }
-        if (exit) {
-            let pw = await bcrypt.hash(req.body.password, 10)
-            console.log(pw)
-            try {
-                await db.query('INSERT INTO USERS VALUES($1,$2,$3,$4,$5,$6)', [body.username, body.password, body.email, body.name, body.lastname, 4])
-            } catch (error) {
-                console.log(error)
-            }
-        }
+        body.username = body.username.trim();
+        body.email = body.email.trim();
+        body.name = body.name.trim();
+        body.lastname = body.lastname.trim();
 
+        let check=body.username.indexOf(" ");
+
+        if(body.username!="" && check<0 && body.email!="" && body.name!="" && body.lastname!=""){
+            let response = await db.query('SELECT USERNAME FROM USERS WHERE USERNAME = $1', [req.body.username])
+            console.log(response.rows)
+
+            let db1 = response.rows;
+
+            var exit = false
+            if (db1[0] === null) {
+                exit = true
+            }
+            if (exit) {
+                let pw = await bcrypt.hash(req.body.password, 10)
+                console.log(pw)
+                try {
+                    await db.query('INSERT INTO USERS VALUES($1,$2,$3,$4,$5,$6)', [body.username, pw, body.email, body.name, body.lastname, 4])
+                } catch (error) {
+                    console.log(error)
+                }
+             }
+        }
     } else {
         res.status(404)
     }
 }
-
