4af28cc05c1e219c6789e870ced54dad06d12bd6
Laura-Martinez182 <lauramartinezo182@gmail.com>
2022-05-21 11:40:08 -0500

    Insert user in DB - register

--- src/pages/api/user/register.js
+++ src/pages/api/user/register.js
@@ -1,8 +1,30 @@
 import bcrypt from 'bcrypt'
+import db from '../../util/database'
 
-export default async function(req, res){
+export default async function register(req, res) {
+    let response = await db.query('SELECT USERNAME FROM USERS WHERE USERNAME = $1', [req.body.username])
+    console.log(response.rows)
 
-    let pw= await bcrypt.hash(req.body.password, 10)
+    let db1 = response.rows;
+    const { method, body } = req;
 
-    console.log(pw)
+    if (method === "POST") {
+        var exit = false
+        if (db1[0] === null) {
+            exit = true
+        }
+        if (exit) {
+            let pw = await bcrypt.hash(req.body.password, 10)
+            console.log(pw)
+            try {
+                await db.query('INSERT INTO USERS VALUES($1,$2,$3,$4,$5,$6)', [body.username, body.password, body.email, body.name, body.lastname, 4])
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+    } else {
+        res.status(404)
+    }
 }
+
