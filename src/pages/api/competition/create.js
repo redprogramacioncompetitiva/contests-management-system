fff760522c0e03a6db6c1876e2135178a13a5049
Carlos Arturo Diaz <70271822+ArturoDiaz02@users.noreply.github.com>
2022-05-21 23:02:15 -0500

    Date Format

--- src/pages/api/competition/create.js
+++ src/pages/api/competition/create.js
@@ -1,14 +1,16 @@
+//import {db} from '../util/database'
+
 export default async function handler(req, res) {
-    
     
     const { method, body } = req;
     const data = body.data;
 
     if (method === 'POST') {
-        
-        console.log(data);
 
-        if(new Date(data.endInscriptionDate).getTime() < new Date(data.startInscriptionDate).getTime() || new Date(data.endDate).getTime() < new Date(data.startDate).getTime()){
+        if(new Date(data.endInscriptionDate).getTime() < new Date(data.startInscriptionDate).getTime() ||
+           new Date(data.endDate).getTime() < new Date(data.startDate).getTime() ||
+           new Date(data.startDate).getTime() < new Date(data.endInscriptionDate).getTime()){
+
             res.send({
                 success: false,
                 message: "Fechas malas"
@@ -24,46 +26,37 @@ export default async function handler(req, res) {
             
         }
 
-        let date = formatDate(data)
+        let date = dateFormat(data)
 
         /*let insert = await db.query('INSERT INTO COMPETITION(NAME,DESCRIPTION,START_INSCRIPTION,END_INSCRIPTION,START_DATE,END_DATE,TEAM_MEMBERS_MIN,TEAM_MEMBERS_MAX) ' +
-                                    'VALUES($1,$2,TO_TIMESTAMP($3),TO_TIMESTAMP($4),TO_TIMESTAMP($5),TO_TIMESTAMP($6),$7,$8)',
-                                    [data.name, data.description, date[0], date[1], date[2], date[3]]);  */          
+                                    'VALUES($1,$2,TO_TIMESTAMP($3),TO_TIMESTAMP($4),TO_TIMESTAMP($5),TO_TIMESTAMP($6),$7,$8, '6')',
+                                    [data.name, data.description, date[0], date[1], date[2], date[3], data.minMembers, data.maxMembers]);  */        
 
         res.send({
             success: true,
             message: "Competencia agregada"
         })
-           
-        
-
-            
-           
+                 
     }        
         
 }
 
-function formatDate(data){
-    const format = 'DD-MM-YYYY HH:MI'
+function dateFormat(data){
+    const format = 'DD-MM-YYYY HH24:MI'
     let array = []
+    let info = null
 
-    let info = new Date(data.startInscriptionDate).toLocaleString().split(", ")
-    info[1] = info[1].replaceAll(".", "").replaceAll(" ", "")
-    array.push("'" + info[0] + " " + info[1] + "','" + format + "'")
+    info = new Date(data.startInscriptionDate)
+    array.push("'" + info.toLocaleString().split(", ")[0] + " " + info.getHours() + ":" + info.getMinutes() + "','" + format + "'")
 
-    info = new Date(data.endInscriptionDate).toLocaleString().split(", ")
-    info[1] = info[1].replaceAll(".", "").replaceAll(" ", "")
-    array.push("'" + info[0] + " " + info[1] + "','" + format + "'")
+    info = new Date(data.endInscriptionDate)
+    array.push("'" + info.toLocaleString().split(", ")[0] + " " + info.getHours() + ":" + info.getMinutes() + "','" + format + "'")
 
-    info = new Date(data.startDate).toLocaleString().split(", ")
-    info[1] = info[1].replaceAll(".", "").replaceAll(" ", "")
-    array.push("'" + info[0] + " " + info[1] + "','" + format + "'")
+    info = new Date(data.startDate)
+    array.push("'" + info.toLocaleString().split(", ")[0] + " " + info.getHours() + ":" + info.getMinutes() + "','" + format + "'")
 
-    info = new Date(data.endDate).toLocaleString().split(", ")
-    info[1] = info[1].replaceAll(".", "").replaceAll(" ", "")
-    array.push("'" + info[0] + " " + info[1] + "','" + format + "'")
-
-    console.log(array)
+    info = new Date(data.endDate)
+    array.push("'" + info.toLocaleString().split(", ")[0] + " " + info.getHours() + ":" + info.getMinutes() + "','" + format + "'")
 
     return array
 
