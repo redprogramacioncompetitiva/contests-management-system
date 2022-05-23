49c61b495c3516aa27c98c05ef4ffbcf4b7d564f
Mateo <mateora73@gmail.com>
2022-05-23 16:40:30 -0500

    Classes update

--- src/model/classes.js
+++ src/model/classes.js
@@ -6,4 +6,19 @@ class Team {
     }
 }
 
-export default {Team}
+class CompetitionPreview{
+    constructor(id, name, description, startInscription,endInscription,startDate, endDate, teamMemMax,teamMemMin,status,venues){
+        this.id = id;
+        this.description = description;
+        this.startInscription = startInscription;
+        this.endInscription = endInscription;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.teamMemMax = teamMemMax;
+        this.teamMemMin = teamMemMin;
+        this.status = status;
+        this.venues = venues;
+    }
+}
+
+export default {Team, CompetitionPreview}
