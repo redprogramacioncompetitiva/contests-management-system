419f7419fc80bef829967985cf56b611321ccf4d
JuanPalta <juanpalta24@hotmail.com>
2022-05-23 22:44:46 -0500

    fix: fix add items, put key to the list

--- src/components/teamsComponents/memberField.js
+++ src/components/teamsComponents/memberField.js
@@ -1,57 +1,83 @@
-import {Component} from "react"
-import styles from '../../styles/createTeam.module.css'
+import { Component } from "react";
+import styles from "../../styles/createTeam.module.css";
 
-export default class extends Component{
-    constructor(props){
-        super(props)
-        this.state = {members : [""]}
+export default class MemberField extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { members: [""] };
+  }
 
+  handleDelete = (e) => {
+    if (this.state.members.length != 1) {
+      let aux = this.state.members;
+      let index = aux.indexOf(e.target.name);
+      aux.splice(index, 1);
+      this.setState({
+        members: aux,
+      });
     }
-    
-    handleDelete = (e) =>{
-        let aux = this.state.members
-        let index = aux.indexOf(e.target.name)
-        aux.splice(index,1)
-        this.setState({
-            members : aux
-        })
-    }
+  };
 
-    handleAdd = (e) =>{
-        let value = e.target.name
-        let empty = value.slice(0,1)
-        if(empty !== " "){
-            empty = value.slice(value.length-1,value.length)
-            if(empty !== " "){
-                if(value.length !== 0){
-                    let aux = this.state.members
-                    aux.push("")
-                    this.setState({
-                        members : aux
-                    })
-                    e.target.disabled = true
-                }
-            } 
+  handleAdd = (e) => {
+    let value = e.target.name;
+    let empty = value.slice(0, 1);
+    if (empty !== " ") {
+      empty = value.slice(value.length - 1, value.length);
+      if (empty !== " ") {
+        if (value.length !== 0) {
+          let aux = this.state.members;
+          aux.push("");
+          this.setState({
+            members: aux,
+          });
+          if (aux.indexOf(value) == aux.length - 2) {
+            e.target.disabled = true;
+          } else {
+            e.target.disabled = false;
+          }
         }
+      }
     }
+  };
 
-    handleChanged = (e) =>{
-        let aux = this.state.members
-        let index = aux.indexOf(e.target.name)
-        aux[index] = e.target.value
-        this.setState({
-            members : aux
-        })
-    }
+  handleChanged = (e) => {
+    let aux = this.state.members;
+    let index = aux.indexOf(e.target.name);
+    aux[index] = e.target.value;
+    this.setState({
+      members: aux,
+    });
+  };
 
-    render(){
-        return(
-        <div>
-            {this.state.members.map((e)=><div>
-            <input type = 'text' onChange={this.handleChanged} name = {e} value = {e}></input>
-            <button type = 'button' name = {e} onClick ={this.handleDelete} className={styles.button}>-</button>
-            <button type = 'button' disabled = {false} name = {e} onClick={this.handleAdd} className={styles.button}>+</button>
-            </div>)}
-        </div>)
-    }
+  render() {
+    const membersList = this.state.members.map((e) => (
+      <div key={this.state.members.indexOf(e)}>
+        <input
+          type="text"
+          onChange={this.handleChanged}
+          name={e}
+          value={e}
+        ></input>
+        <button
+          type="button"
+          name={e}
+          disabled={false}
+          onClick={this.handleDelete}
+          className={styles.button}
+        >
+          -
+        </button>
+        <button
+          type="button"
+          disabled={false}
+          name={e}
+          onClick={this.handleAdd}
+          className={styles.button}
+        >
+          +
+        </button>
+      </div>
+    ));
+    return <div>{membersList}</div>;
+  }
 }
