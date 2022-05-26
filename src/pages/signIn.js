eb54713169d9107dc9644fd5be3b6e4019190e35
SeanQO <seanquintero3@gmail.com>
2022-05-26 15:35:38 -0500

    added logo, and base sign in

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -1,38 +1,34 @@
-import React from "react";
-import {Providers, signIn, getSession, csrfToken} from "next-auth"
+import { Container } from "@mui/material"
+import { getCsrfToken } from "next-auth/react"
 
-export default function signIn({Providers,csrfToken}){
-    return <container>
-        <Heading as="h1" textAling="center">
-            RPC
-        </Heading>
-        <Box alignContent="center" justifyContent="center" marginTop={12}>
-            <Box className="signIn-form">
-                <form method="post" action="/api/auth/signin/user">
-                    <Input name="credentials" type="hidden" defaultValue={Credentials}></Input>
-                    <label> Email
-                    <Input type="text" id="email" name="email"/>
-                    </label>
-                    <Button type="submit"> </Button>
-                </form>
-            </Box>
-        </Box>
-    </container>
+export default function SignIn({ csrfToken }) {
+    return (
+        <Container>
+            <div>
+              <img src="images/logo.png" width={50} height={50} />
+              <h1>Sign In</h1>
+            </div>
+            <form method="post" action="/api/auth/callback/credentials">
+                <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+                <label>
+                    Username
+                    <input name="username" type="text" />
+                </label>
+                <label>
+                    Password
+                    <input name="password" type="password" />
+                </label>
+                <button type="submit">Sign in</button>
+            </form>
+        </Container>
+
+    )
 }
 
-signIn.getInitialProps = async(context) =>{
-    const{req,res} = context;
-    const session = await getSession({req});
-    
-    if(session && res && session.accessToken){
-        res.writehead(302,{
-            location:"/"
-        });
-        res.end()
-        return;
-    }
-    return{
-        session: undefined,
-        providers: await providers(context)
+export async function getServerSideProps(context) {
+    return {
+        props: {
+            csrfToken: await getCsrfToken(context),
+        },
     }
 }
