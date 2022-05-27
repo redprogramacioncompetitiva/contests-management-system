2f29969294caf92cbbe23a33f7e0259f17317998
SeanQO <seanquintero3@gmail.com>
2022-05-27 03:19:17 -0500

    added format to sign in form

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -4,22 +4,27 @@ import { getCsrfToken } from "next-auth/react"
 export default function SignIn({ csrfToken }) {
     return (
         <Container>
-            <div>
-              <img src="images/logo.png" width={50} height={50} />
-              <h1>Sign In</h1>
+            <div class="sign-in-view">
+                <div class="main-container">
+                    <div class="form-signin-container sign-in-container">
+                        <form action="#">
+                            <h1>Sign in</h1>
+                            <span>Use your account</span>
+                            <input class="sign-in-input" type="email" placeholder="Email" />
+                            <input class="sign-in-input" type="password" placeholder="Password" />
+                            <button>Sign In</button>
+                        </form>
+                    </div>
+                    <div class="overlay-signin-container">
+                        <div class="signin-overlay">
+                            <div class="signin-overlay-panel">
+                                <img src="images/logo.png" width={250} height={250} />
+                                <h1 class="sub-header">RPC:: RED DE PROGRAMACIÓN COMPETITIVA</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <form method="post" action="/api/auth/callback/credentials">
-                <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-                <label>
-                    Username
-                    <input name="username" type="text" />
-                </label>
-                <label>
-                    Password
-                    <input name="password" type="password" />
-                </label>
-                <button type="submit">Sign in</button>
-            </form>
         </Container>
 
     )
