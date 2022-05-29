d6bf2d9677cdddd6da5ae11afc68902f847462ed
SeanQO <seanquintero3@gmail.com>
2022-05-28 20:36:09 -0500

    fixed prev commit

--- src/pages/signIn.js
+++ src/pages/signIn.js
@@ -5,27 +5,17 @@ import { getCsrfToken } from "next-auth/react"
 export default function SignIn({ csrfToken }) {
     return (
         <Container>
-            <div class="sign-in-view">
+            <div class='overlay'>
                 <div class="main-container">
-                    <div class="form-signin-container sign-in-container">
-                        <form class="sign-in-form" action="/api/auth/callback/credentials">
-                            <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-                            <h1>Sign in</h1>
-                            <span>Use your account</span>
-                            <input class="sign-in-input" type="email" placeholder="Email" name="email"/>
-                            <input class="sign-in-input" type="password" placeholder="Password" name="password"/>
-                            <hr/>
-                            <button class="sign-in-button">Sign In</button>
-                        </form>
-                    </div>
-                    <div class="overlay-signin-container">
-                        <div class="signin-overlay">
-                            <div class="signin-overlay-panel">
-                                <img src="images/logo.png" width={250} height={250} />
-                                <h1 class="sub-header">RPC:: RED DE PROGRAMACIÓN COMPETITIVA</h1>
-                            </div>
-                        </div>
-                    </div>
+                    <form class="sign-in-form" action="/api/auth/callback/credentials">
+                        <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+                        <h1  class="sigin-h1">INICIAR SESIÓN</h1>
+                        <span>¿No tienes una cuenta? <a class="sign-a" href="/register">Registrarse</a> </span>
+                        <input class="sign-in-input" type="email" placeholder="Email" name="email" />
+                        <input class="sign-in-input" type="password" placeholder="Password" name="password" />
+                        <hr />
+                        <button class="sign-in-button">Sign In &#8594;</button>
+                    </form>
                 </div>
             </div>
         </Container>
