dd95fc0c92c4c0418d708a4ca0bac212821e7a9c
Julian Bolanos <julian.camilob24@gmail.com>
2022-05-30 14:55:40 -0500

    Example request for competition and inserts file fixed

--- src/pages/api/selectExample.js
+++ src/pages/api/selectExample.js
@@ -1,10 +1,50 @@
 import database from "../../util/database";
 
 export default async function handle(req) {
+    
+    var idCompetition = '4';
+    
     switch (req.method){
         case "GET": res.statusCode = 404; break;
         case "POST":
-            let response = await database.query('SELECT * FROM USERS WHERE USER_TYPE = 2')
-            console.log(response.rows)
+            var responseCompetitionInfo = await database.query(
+                `SELECT ID_COMPETITION, NAME, DESCRIPTION, TEAM_MEMBERS_MAX 
+                FROM COMPETITION 
+                WHERE ID_COMPETITION = ${idCompetition}`)
+
+            var competitionInfo = responseCompetitionInfo.rows[0];
+
+            var responseVenueCompetition = await database.query(
+                `SELECT ID_VENUE FROM VENUE_COMPETITION
+                WHERE ID_COMPETITION = ${idCompetition};`
+            )
+
+            var idVenue = responseVenueCompetition.rows[0].id_venue;
+
+            var responseVenue = await database.query(
+                `SELECT SHORT_NAME, CITY_CODE FROM VENUE
+                WHERE ID_VENUE = ${idVenue};`
+            )
+
+            var shortName = responseVenue.rows[0].short_name;
+            var cityCode = responseVenue.rows[0].city_code;
+
+            var responseCity = await database.query(
+                `SELECT CITY_NAME, CITY_CODE FROM CITY
+                WHERE CITY_CODE = '${cityCode}';`
+            );
+
+            var cityInfo = responseCity.rows[0]
+
+            var details = {
+                name : competitionInfo.name,
+                description : competitionInfo.description,
+                teamMembersMax : competitionInfo.team_members_max,
+                institution : shortName,
+                city : cityInfo.city_name
+            };
+
+            console.log(details);
+
     }
 }
