dcf2b849faee4d97bb3f2a1f6f34957e792b825a
DaniloErazo <erazodanilo1218@gmail.com>
2022-05-30 20:30:29 -0500

    Added validation for enrollmet: users already registered in competition with other team

--- src/pages/api/team/enrollment.js
+++ src/pages/api/team/enrollment.js
@@ -10,9 +10,11 @@ export default async function handle(req, res){
         FROM TEAM
         WHERE ID_TEAM = '${idTeam}'`
     )
-   let leaderValidation = teamLeader.rows[0].team_leader === username
 
-   let competitorsNumber = true;
+    //Validate if the user who's trying to join the competititon is the leader
+    let leaderValidation = teamLeader.rows[0].team_leader === username
+
+    let competitorsNumber = true;
 
     var maxCompetitors = await database.query(
         `SELECT TEAM_MEMBERS_MAX
@@ -25,12 +27,43 @@ export default async function handle(req, res){
         WHERE ID_TEAM = '${idTeam}'`
     )
     
-    console.log(maxCompetitors.rows[0], teamCompetitors.rows[0])
+    //console.log(maxCompetitors.rows[0], teamCompetitors.rows[0])
+    //Validate if the team doesn't exceed the maximum of competitors accepted in the competititon
     if (teamCompetitors.rows[0].count<maxCompetitors.rows[0].team_members_max) {
         competitorsNumber = false;
     }
 
-    console.log(competitorsNumber)
+    var teamCompetitors = await database.query(
+        `SELECT USERNAME
+        FROM USERS_TEAM
+        WHERE ID_TEAM = '${idTeam}'`
+    )
+
+    // Inser for testing next validation: INSERT INTO USER_TEAM_COMPETITION (USERNAME, ID_TEAM, ID_COMPETITION) VALUES ( 'pepito' , 'TM000001', 004 );
+    
+
+    let alreadyInCompetition = true;
+    validation()
+    //Validate if any of the users is already registered in the same competition
+    async function validation (){
+        for (let index = 0; index < teamCompetitors.rows.length ; index++) {
+                console.log(teamCompetitors.rows[index])
+                let alreadyTeam = await database.query(
+                    `SELECT ID_TEAM 
+                    FROM USER_TEAM_COMPETITION
+                    WHERE USERNAME = '${teamCompetitors.rows[index].username}' 
+                    AND ID_COMPETITION = ${idCompetition}`
+                )
+                
+                if(alreadyTeam.rowCount!==0){
+                    alreadyInCompetition = false
+                    break
+                    
+                }
+                
+            }
+    }
+
 
     res.send("someting")
 }
