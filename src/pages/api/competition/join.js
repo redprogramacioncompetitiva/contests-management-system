e62636539a17414a93f81b78f2fc16b0c476e7b5
Santiago Gutiérrez <santicoguvi02@gmail.com>
2022-05-31 01:52:30 -0500

    Insert in the User team competition table.

--- src/pages/api/competition/join.js
+++ src/pages/api/competition/join.js
@@ -3,20 +3,19 @@ import {db} from '../../../util/database';
 export default async (req, res) => {
 
     const {method, body} = req;
-    const idTeam = body.teamId;
 
+
+    const idTeam = body.teamId;
+    const idCompetition = body.competitionId;
 
     switch(method){
         case 'GET': res.statusCode = 404; break;
 
         case 'POST':
-        const team = await db.query('SELECT USERNAME FROM USERS_TEAM WHERE ID_TEAM = ${idTeam}');
+        const members = await db.query('SELECT USERNAME FROM USERS_TEAM WHERE ID_TEAM = ${idTeam}');
 
-        for (let index = 0; index < team.rows.length; index++){
-            var members = [];
-            members[index] = team.rows[index];
+        for (let index = 0; index < members.rows.length; index++){
+            await db.query('INSERT INTO USER_TEAM_COMPETITION VALUES($1, $2, $3)', [members.rows[index].username, idTeam, idCompetition])
         }
-
     }
-
-}
+};
