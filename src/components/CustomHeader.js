228c09070d866a072587f0bcdf986c073cc852fe
KennetSanchez <kesasaro@gmail.com>
2022-05-31 15:04:57 -0500

    Competitions page updated
    * The competitions page show the competitions divided in the ones that the user can enroll, and the ones where he can't.
    
    * Box shadow updated
    
    * Promp navbar added (just for decoration)

--- src/components/CustomHeader.js
+++ src/components/CustomHeader.js
@@ -18,8 +18,8 @@ export default class CustomHeader extends Component{
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
                 <link rel="stylesheet" href="../../styles/TeamsItems.module.css"/>
-              
-            </Head>
+                            
+            </Head>          
         )
     }
 }
--- src/pages/users/[userId]/index.js
+++ src/pages/users/[userId]/index.js
@@ -13,41 +13,111 @@ function userIndex({ data }) {
 
   if (session && session.username == router.query.userId) {
     return (
-      <div id={style.competitionContainer}>
-        <CustomHeader title = "Competencias">
-        </CustomHeader>
+      <div>
+        <nav
+          id={style.navBar}
+          className="navbar navbar sticky-top navbar-expand-lg navbar-dark bg-dark"
+        >
+          <a className="navbar-brand" href="#">
+            RPC
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">
+                  Inicio <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Competencias
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    class="bi bi-bell-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z" />
+                  </svg>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    class="bi bi-person-circle"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                    <path
+                      fill-rule="evenodd"
+                      d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                    />
+                  </svg>
+                </a>
+              </li>             
+            </ul>
+          </div>
+        </nav>
 
-        <section id={style.competitionsEnabled}>
-        <h2>Competencias en las que puedo participar</h2>
-        <div>
-          {data.competitions.map((e) => (
-            <CompetitionItem className = "card"
-              competitionName={e.name}
-              description={e.description}
-              competitionStartDate={e.startInscription}
-              competitionEndDate={e.endInscription}
-              competitionEnrollStartDate={e.startDate}
-              competitionEnrollEndDate={e.endDate}
-            />
-          ))}
-        </div>
-        </section>
+        <div id={style.competitionContainer}>
+          <CustomHeader title="Competencias"></CustomHeader>
 
-        <section id={style.competitionsDisabled}>
-        <h2>Competencias en las que <strong>no</strong> puedo participar</h2>
-        <div>
-          {data.competitions.map((e) => (
-            <CompetitionItem className = "card"
-              competitionName={e.name}
-              description={e.description}
-              competitionStartDate={e.startInscription}
-              competitionEndDate={e.endInscription}
-              competitionEnrollStartDate={e.startDate}
-              competitionEnrollEndDate={e.endDate}
-            />
-          ))}
+          <section id={style.competitionsEnabled}>
+            <h2>Competencias en las que puedo participar</h2>
+
+            <div>
+              {data.competitionsEnabled.map((e) => (
+                <CompetitionItem
+                  competitionName={e.name}
+                  description={e.description}
+                  competitionStartDate={e.startInscription}
+                  competitionEndDate={e.endInscription}
+                  competitionEnrollStartDate={e.startDate}
+                  competitionEnrollEndDate={e.endDate}
+                />
+              ))}
+            </div>
+          </section>
+
+          <section id={style.competitionsDisabled}>
+            <h2>
+              Competencias en las que <strong>no</strong> puedo participar
+            </h2>
+            <div>
+              {data.competitionsDisabled.map((e) => (
+                <CompetitionItem
+                  competitionName={e.name}
+                  description={e.description}
+                  competitionStartDate={e.startInscription}
+                  competitionEndDate={e.endInscription}
+                  competitionEnrollStartDate={e.startDate}
+                  competitionEnrollEndDate={e.endDate}
+                />
+              ))}
+            </div>
+          </section>
         </div>
-        </section>
       </div>
     );
   } else {
