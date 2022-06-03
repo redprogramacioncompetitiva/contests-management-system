4efbcc33dd0e8d411bc34029fb97c6cff18e081e
JuanFeBlancoT <juanfeblancot@gmail.com>
2022-06-03 00:33:50 -0500

    id_institution added to team to prevent null values

--- src/pages/api/team/teamInsert.js
+++ src/pages/api/team/teamInsert.js
@@ -5,24 +5,50 @@ export default async function handler(req, res) {
     const id = req.query;
     if (method === "POST") {
         const teamInfo = body;
-        const id_team = "12345678";
+        //Generate a random code
+        const ASCII_FOR_A= 65;
+        const MAX_NUMERIC_PART_NUM = 999999;
+
+        let takenId = false;
+        let letter1 = "";
+        let letter2 = "";
+        let numPart = 0;
+        let id_team = letter1 + letter2 + numPart;
+
+        //Generate random id for team and verifying it isn't taken yet
+        do {
+          letter1 = String.fromCharCode((Math.floor(Math.random()*26)) + ASCII_FOR_A);
+          letter2 = String.fromCharCode((Math.floor(Math.random()*26)) + ASCII_FOR_A);
+          numPart = (Math.floor(Math.random()*MAX_NUMERIC_PART_NUM));
+          id_team = letter1 + letter2 + numPart;
+          //
+          let PossibleIdCollisionData = await db.query("SELECT id_team FROM TEAM WHERE id_team LIKE '%"+ id_team+"%';");
+          let PossibleIdCollision = PossibleIdCollisionData.rows;
+          if(PossibleIdCollision.length != 0){
+            takenId = true;
+          }else{
+            takenId = false;
+          }
+        } while (takenId);
+
         const teamNameFromData = teamInfo.tm.teamName;
-        const values = [ id_team, teamNameFromData, "pepito"];
-        
-        console.log(teamNameFromData);
-        //const query = 'INSERT INTO TEAM(ID_TEAM,TEAM_NAME,LEADER_USERNAME) VALUES ($1,$2,$3) RETURNING ID_TEAM';
-              //try {
-        //const response = await db.query(query,values);
-        //return res.status(200).json(response.rows[0]);
-              /*} catch (e) {
-                  return res.status(400).json({message: e.message});
-              }*/
+        const username_leader = "pepito";
+        const id_institution = 1;
+        const values = [ id_team, teamNameFromData, username_leader, id_institution];
+      
+        //Create team
+        const query = 'INSERT INTO TEAM(ID_TEAM,TEAM_NAME,LEADER_USERNAME,ID_INSTITUTION) VALUES ($1,$2,$3,$4) RETURNING ID_TEAM';
+                    //try {
+        const response = await db.query(query,values);
+                    //return res.status(200).json(response.rows[0]);
+                    /*} catch (e) {
+                        return res.status(400).json({message: e.message});
+                    }*/
+
+        //Add members to team if they exist
         let registeredUsersData = await db.query("SELECT s.username FROM users s");
         let registeredUsers = registeredUsersData.rows;
         
-        console.log(registeredUsers[0].username);
-
-        console.log(teamInfo.tm.members);
         for (let i = 0; i < teamInfo.tm.members.length; i++) {
           let foundINdex = registeredUsers.findIndex(element => element.username == teamInfo.tm.members[i]);
           if(foundINdex == -1){
@@ -32,6 +58,7 @@ export default async function handler(req, res) {
             const valuesMember =[teamInfo.tm.members[i], id_team];
             const query = 'INSERT INTO USERS_TEAM(USERNAME, ID_TEAM) VALUES ($1,$2)';
             const response = await db.query(query,valuesMember);
+            
             //return res.status(200).json(response.rows[0]);
           }
         }
